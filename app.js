ea245a0209c51971ce50949efce622ecbe95ff7c
yusuf <gidado4yusuf@gmail.com>
2022-11-29 15:29:39 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,79 @@
+// var data = 10;
+// var score = 30;
+// var sum = data + score;
+
+// document.write(sum);
+// console.log(sum);
+
+
+// var text1 = "Yusuf ";
+// var text2 = "Gidado";
+// var word = text1 + text2;
+
+// document.write("</br>" + word);
+
+
+// let x = 2 + 3 + "5";
+
+
+// document.write("</br>" + x);
+
+
+// let x = 5;
+// let y = 10;
+
+// let z = x + y;
+
+// let numbers = 100;
+// numbers **=2;
+
+
+
+// document.write(numbers)
+
+// let a = 20;
+// let b = 10;
+// c = a !==  b;
+
+
+// document.write(c);
+// console.log(c);
+// var car = {
+//     product: 'lexus',
+//     type: 'lexus350',
+//     year: 2022,
+    
+// }
+// console.log(car);
+
+
+// var x = 2;     
+// var x = 3;     
+// x = 4;     
+
+// console.log(x);
+
+// function myFunction() {
+//     alert("Hello World!");
+//   }
+
+//   document.write();
+// var num1 = 5;
+// var num2 = 10;
+
+// function toCentimeters(kilometers) {
+//   return (num1/100000);
+// }
+
+// document.write(toCentimeters/100000);
+
+
+// function lengthconverter (valNum)
+// {
+//   document.getElementById(outoutMeters).innerHTML = valNum / 0.0022046;
+// }
+
+let convert = 200/100;
+let output = convert/1000;
+output = convert/100000;
+console.log(output);
